57a0a978ee9eabca532a4fc11bc50bbed71d6d00
llalov <luchoazz@abv.bg>
2015-11-04 02:23:36 +0200

    Initial commit

---
+++ EventManager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventManager.Web.Startup))]
+namespace EventManager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
